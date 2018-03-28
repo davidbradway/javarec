78629347a6b2daa58468f7a38972037f7350351a
David Bradway <dpb6@duke.edu>
2018-03-28 11:26:25 -0400

    add code for First Ratings Rater class

--- StepOneStarterProgram/FirstRatings.java
+++ StepOneStarterProgram/FirstRatings.java
@@ -177,27 +177,200 @@ public class FirstRatings {
      * loadRaters
      */
     private ArrayList<Rater> loadRaters(String filename) {
+        //process every record from the CSV file whose name is filename,
+
+        FileResource fr = new FileResource(filename);
+        
+        ArrayList<Rater> raterlist = new ArrayList<Rater>();
+        //rater_id,movie_id,rating,time
+        for (CSVRecord rec : fr.getCSVParser(true)) {
+            String rater_id = rec.get(0);
+            String movie_id = rec.get(1);
+            Double rating = Double.parseDouble(rec.get(2));
+            String time = rec.get(3); // we don't use this
+
+            // see if rater_id is already in raterlist
+            int index = 0;
+            boolean notFound = true;
+            while (index < raterlist.size() && notFound) {
+                Rater r = raterlist.get(index);
+            
+                if (r.getID().equals(rater_id.trim())) {
+                    // if rater_id is already in raterlist,
+                    notFound = false;
+
+                    // check if the item is already rated.
+                    if (r.hasRating(movie_id) == false) {
+                        // we should add the rating to the entry
+                        r.addRating(movie_id, rating);
+
+                        // break rest of loop
+                    }
+                    raterlist.set(index,r);
+                    break;
+                }
+                
+                // look at the nest Rater in the raterlist
+                index++;
+            }
+            //if not found in loop, then rater_id is not yet in raterlist
+            if (notFound) {
+                Rater r = new Rater(rater_id);
+                r.addRating(movie_id, rating);
+                raterlist.add(r);
+            }
+        }
+        
         /*
-         * process every record from the CSV file whose name is filename,
-         * a file of raters and their ratings,
-         * and return an ArrayList of type Rater
+         * return an ArrayList of type Rater
          * with all the rater data from the file.
          */
+        return raterlist;
+    }
+    
+    /**
+     * Find number of ratings by a rater
+     */
+    private int numRatingsbyRater(ArrayList<Rater> raterlist, String id) {
+        // check the whole list if necessary
+        for (Rater r : raterlist) {
+            // for each Rater, check the ID
+            if (r.getID().equals(id)) {
+                // if it's a match, return the number of rated items
+                return r.getItemsRated().size();
+            }
+        }
+        // if we didn't find that user, return 0;
+        return 0;
+    }
+
+    /**
+     * Max num ratings
+     */
+    private HashMap<Integer, ArrayList<String>> maxNumRatings(ArrayList<Rater> raterlist) {
+        /* find the maximum number of ratings by any rater. 
+         * Determine how many raters have this maximum number of ratings 
+         * and who those raters are. */
+
+        int maxcount = 0;
+        ArrayList<String> ratersmax = new ArrayList<String>();
+
+        // iterate through entire raterlist
+        for (Rater r : raterlist) {
+            //get Ratings
+            int numRated = r.getItemsRated().size();
+            if (numRated > maxcount) {
+                // found a new max
+                maxcount = numRated;
+                ratersmax.clear();
+                ratersmax.add(r.getID());
+            }
+            else if (numRated == maxcount){
+                // found a tie with old max
+                ratersmax.add(r.getID());
+            }
+        }
+
+        HashMap<Integer, ArrayList<String>> maxMap = new HashMap<Integer, ArrayList<String>>();
+        maxMap.put(maxcount, ratersmax);
+        return maxMap;
+    }
+    
+    /**
+     * Find all the raters of a given movie
+     */
+    private ArrayList<String> getRatersByMovie(ArrayList<Rater> raterlist, String movie_id) {
+        ArrayList<String> raters = new ArrayList<String>();
         
-        return null;
+        // iterate through entire raterlist
+        for (Rater r : raterlist) {
+            //get Ratings
+            ArrayList<String> items = r.getItemsRated();
+            if (items.contains(movie_id)) {
+                raters.add(r.getID());
+            }
+        }
+        
+        return raters;
+    }
+    
+    /**
+     * Unique movies by all     raters
+     */
+    private ArrayList<String> uniqueMovies(ArrayList<Rater> raterlist) {
+        ArrayList<String> movies = new ArrayList<String>();
+        
+        // iterate through entire raterlist
+        for (Rater r : raterlist) {
+            //get Movies
+            ArrayList<String> items = r.getItemsRated();
+            // add any that aren't in the movies list yet
+            for (String item : items) {
+                if (!movies.contains(item)) {
+                    movies.add(item);
+                }
+            }
+        }
+        return movies;
     }
     
     /**
      * testLoadRaters
      */
-    private void testLoadRaters() {
-        ArrayList<Rater> raterlist = loadRaters("ratings_short.csv");
-        /*
-         * Print the total number of raters. Then for each rater, print the rater’s ID and the number of ratings they did on one line, followed by each rating (both the movie ID and the rating given) on a separate line. If you run your program on the file ratings_short.csv you will see there are five raters. After it looks like it works, you may want to comment out the printing of each rater and their ratings. If you run your program on the file ratings.csv, you should get 1048 raters.
-         * Add code to find the number of ratings for a particular rater you specify in your code. For example, if you run this code on the rater whose rater_id is 2 for the file ratings_short.csv, you will see they have three ratings.
-         * Add code to find the maximum number of ratings by any rater. Determine how many raters have this maximum number of ratings and who those raters are. If you run this code on the file ratings_short.csv, you will see rater 2 has three ratings, the maximum number of ratings of all the raters, and that there is only one rater with three ratings.
-         * Add code to find the number of ratings a particular movie has. If you run this code on the file ratings_short.csv for the movie “1798709”, you will see it was rated by four raters.
-         * Add code to determine how many different movies have been rated by all these raters. If you run this code on the file ratings_short.csv, you will see there were four movies rated.
-         */
+    public void testLoadRaters() {
+        ArrayList<Rater> raterlist = loadRaters("data/ratings_short.csv");
+        //ArrayList<Rater> raterlist = loadRaters("data/ratings.csv");
+        System.out.println("Total number of raters: " + raterlist.size());
+        
+        // Then for each rater, print rater’s ID and number of ratings on one line, 
+        // followed by each rating (both movie ID and rating given) on a separate line.
+        // If you run your program on the file ratings_short.csv there are five raters.
+        /*for (Rater r : raterlist) {
+            // process each item in turn 
+            System.out.println("ID:" + r.getID() + " #ratings: " + r.getItemsRated().size());
+
+            ArrayList<String> ratings = r.getItemsRated();
+            for (String item : ratings) {
+                System.out.println(item + " : " + r.getRating(item));
+            }
+        }*/
+        // After it looks like it works, you may want to comment out 
+        // the printing of each rater and their ratings.
+
+        // If you run your program on the file ratings.csv, you should get 1048 raters.
+
+        // find the number of ratings for a particular rater.
+        // For example, if you run this code on the rater whose rater_id is 2 
+        // for the file ratings_short.csv, you will see they have three ratings. */
+        int num = numRatingsbyRater(raterlist, "2");
+        System.out.println("Number of ratings by the given rater: " + num);
+        
+        // find the maximum number of ratings by any rater. 
+        // Determine how many raters have this maximum number of ratings 
+        // and who those raters are. 
+        // If you run this code on the file ratings_short.csv, 
+        // you will see rater 2 has three ratings, 
+        // the maximum number of ratings of all the raters, 
+        // and that there is only one rater with three ratings. */
+        HashMap<Integer, ArrayList<String>> maxMap = maxNumRatings(raterlist);
+        //System.out.println(maxMap);
+        for (Integer number : maxMap.keySet()) {
+            System.out.println("Max Number of ratings by any given rater: "+ number);
+            ArrayList<String> raters = maxMap.get(number);
+            //System.out.println("Number of raters who have rated that many movies: " + raters.size());
+            System.out.println("IDs of raters that have that many ratings: " + raters);
+        }
+        
+        // find the number of ratings a particular movie has.
+        // If you run this code on the file ratings_short.csv for the movie “1798709”,
+        // you will see it was rated by four raters.
+        ArrayList<String> raters = getRatersByMovie(raterlist, "1798709");
+        System.out.println("Given movie was rated by " + raters.size() + " raters");
+        
+        // Determine how many different movies have been rated by all these raters.
+        // If you run this code on the file ratings_short.csv, you will see there were
+        // four movies rated.
+        ArrayList<String> movies = uniqueMovies(raterlist);
+        System.out.println("Movies rated: " + movies.size());
     }
 }
